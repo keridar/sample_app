6ef957d4765ef1461d52440fefcd87c89dea85d0
Keridar <keridar@gmail.com>
2011-06-20 14:59:49 -0500

    Added Help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    
+    @base_title = "Ruby on Rails Tutorial Sample App"
+    
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,7 +17,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end  
     
     it "should have a non-blank body" do
@@ -27,10 +33,15 @@ describe PagesController do
       response.should be_success
     end
     
-     it "should have the right title" do
+    it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
       end
+      
+    it "should have a non-blank body" do
+      get 'contact'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
   
   describe "GET 'about'" do
@@ -39,10 +50,32 @@ describe PagesController do
       response.should be_success
     end
     
-     it "should have the right title" do
+    it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title + " | About")
       end
+      
+    it "should have a non-blank body" do
+      get 'about'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + " | Help")
+      end
+      
+    it "should have a non-blank body" do
+      get 'help'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
   
 end
