578a4e8d6cb68b6ae0602478322f2adfeaadaeda
Keridar <keridar@gmail.com>
2011-07-06 16:07:36 -0500

    Can not destroy users (As admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-	before_filter :authenticate, :only => [:index, :edit, :update]
+	before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
 	before_filter :correct_user, :only => [:edit, :update]
+	before_filter :admin_user,   :only => :destroy
 	
 	def index
 		@users = User.paginate(:page => params[:page])
@@ -32,12 +33,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
   
   def update
-  	@user = User.find(params[:id])
   	if @user.update_attributes(params[:user])
   		flash[:success] = "Profile updated."
   		redirect_to @user
@@ -46,6 +45,11 @@ class UsersController < ApplicationController
   		render 'edit'
   	end
   end  
+  
+  def destroy
+  	User.find(params[:id]).destroy
+  	redirect_to users_path, :flash => { :success => "User destroyed."}
+	end
   
   private
   
@@ -56,6 +60,11 @@ class UsersController < ApplicationController
   	def correct_user
 			@user = User.find(params[:id])
   		redirect_to(root_path) unless current_user?(@user)
+  	end
+  	
+  	def admin_user
+  		user = User.find(params[:id])
+			redirect_to(root_path) if	(!current_user.admin? || current_user?(user))
   	end
 
 end
